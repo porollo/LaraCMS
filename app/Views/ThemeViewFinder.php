cb1e581d9715560ee69da3010f7d408c0cbcc0b1
porollo <d.porollo@gmail.com>
2021-08-10 00:20:32 +0300

    Update

---
+++ app/Views/ThemeViewFinder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Views;
+
+use Illuminate\View\FileViewFinder;
+
+class ThemeViewFinder extends FileViewFinder
+{
+    protected $activeTheme;
+    protected $basePath;
+
+    public function setBasePath($path) {
+        $this->basePath = $path;
+    }
+
+    public function setActiveTheme($theme) {
+        $this->activeTheme = $theme;
+        array_unshift($this->paths, $this->basePath.'/'.$theme.'/views');
+    }
+}
---
+++ public/themes/default/views/layouts/backend.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/themes/default/assets/css/default.css">
+        <title></title>
+    </head>
+    <body>
+        <h1>LaraCMS</h1>
+    </body>
+</html>
